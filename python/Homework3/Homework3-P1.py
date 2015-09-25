d9309165824d5019b685a3baeeb0727407938c3c
bmarshall <bmarshallpf@gmail.com>
2015-09-24 21:25:21 -0400

    homework 3 first upload

---
+++ python/Homework3/Homework3-P1.py
@@ -0,0 +1,25 @@
+﻿#Brandon Marshall       
+#Python Scripting
+#September 24, 2015
+#Homework 3 - Problem 1
+
+# We have bunnies standing in a line, numbered 1, 2, ... 
+# The odd bunnies (1, 3, ..) have the normal 2 ears. 
+# The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. 
+# Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication). 
+
+# bunnyEars2(0) ? 0
+# bunnyEars2(1) ? 2
+# bunnyEars2(2) ? 5
+
+def bunny_ears(bunnies):
+	if (bunnies == 0):
+		return 0
+	elif (bunnies % 2 == 0):
+		return 2 + bunny_ears(bunnies - 1)
+	else:
+		return 3 + bunny_ears(bunnies - 1)
+
+print(bunny_ears(0))
+print(bunny_ears(1))
+print(bunny_ears(2))
---
+++ python/Homework3/Homework3-P2.py
@@ -0,0 +1,21 @@
+﻿#Brandon Marshall       
+#Python Scripting
+#September 24, 2015
+#Homework 3 - Problem 2
+
+# Write a function count_frequency that takes a list of words as an argument, counts how many 
+# times each word appears in the list, and then returns this frequency listing as a Python dictionary
+# Sample function call and output:
+
+def count_frequency(mylist):
+	output = {}
+	for word in mylist:
+		if word in output.keys():
+			output[word] = output[word] + 1
+		else:
+			output[word] = 1
+
+	return output
+
+mylist=["one", "two", "eleven",  "one", "three", "two", "eleven", "three", "seven", "eleven"]
+print(count_frequency(mylist))
