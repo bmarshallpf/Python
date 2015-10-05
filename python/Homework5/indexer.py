944dff7ff77f09afa258eec55ce2f01abaf13577
bmarshall <bmarshallpf@gmail.com>
2015-10-04 22:20:42 -0400

    homework 5 tasks 2 and 3

---
+++ python/Homework5/indexer.py
@@ -0,0 +1,41 @@
+﻿#Brandon Marshall       
+#Python Scripting
+#October 1, 2015
+#Homework 4 - File Traverser
+
+import pickle
+import shelve
+
+def process_data(shelve_file, shelve_key, data_pickle, web_pickle = ""):
+	indexed = {}
+
+	f = open(data_pickle, "br")
+	foundData = pickle.load(f)
+	f.close()
+
+	for file in foundData:
+		path = file[0][0]
+		contents = file[0][1]
+		words = tuple(contents.split())
+		for word in words:
+			if word not in indexed.keys():
+				indexed[word] = set()
+
+			indexed[word].add(path)
+
+	if web_pickle != "":
+		f = open(web_pickle, "br")
+		foundData = pickle.load(f)
+		f.close()
+
+		for data in foundData:
+			url = data[0]
+			words = tuple(data[1].split())
+			for word in words:
+				if word not in indexed.keys():
+					indexed[word] = set()
+
+				indexed[word].add(url)
+
+	s = shelve.open(shelve_file)
+	s[shelve_key] = indexed
---
+++ python/Homework5/searcher.py
@@ -0,0 +1,68 @@
+﻿#Brandon Marshall       
+#Python Scripting
+#October 1, 2015
+#Homework 4 - File Traverser
+
+import time
+import shelve
+
+def search(shelve_to_process, shelve_key):
+
+	# start timing of search functionality
+	start_time = time.perf_counter() * 1000
+
+	s = shelve.open(shelve_to_process)
+
+	indexedFiles = s[shelve_key]
+
+	keepGoing = True
+	while keepGoing:
+		query = input("query (type . to exit):")
+		if query == '.':
+			keepGoing = False
+		else:
+			terms = query.split(" ")
+
+			hasOr = False
+			hasAnd = "and" in terms
+			if not hasAnd:
+				hasOr = "or" in terms
+				if not hasOr:
+					hasAnd = True
+
+			#remove the delimiter terms now
+			while "and" in terms:
+				terms.remove("and")
+			while "or" in terms:
+				terms.remove("or")
+
+			goodFiles = set()
+
+			if hasOr:
+				for term in terms:
+					goodFiles = goodFiles.union(indexedFiles[term])
+
+			if hasAnd:
+				for term in terms:
+					if term in indexedFiles:
+						if len(goodFiles) == 0:
+							goodFiles.update(indexedFiles[term])
+						else:
+							goodFiles = goodFiles.intersection(indexedFiles[term])
+
+			# finish timing of search functionality
+			end_time = time.perf_counter() * 1000
+
+			# print the output
+			word = "OR" if hasOr else "AND"
+
+			print("Performing " + word + 
+			   " search for: {\'" + '\', \''.join(terms) + "\'}")
+
+			if len(goodFiles) == 0:
+				print("Sorry, no files found with that query!")
+			else:
+				for gf in goodFiles:
+					print("Found at", gf)
+
+			print("Execution time:", int(end_time - start_time))
