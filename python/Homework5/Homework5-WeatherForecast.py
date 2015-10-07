8c16ef91ffe21bfe3be08bf915d54a4787818ed2
bmarshall <bmarshallpf@gmail.com>
2015-10-07 17:51:39 -0400

    formatting weather forecast

--- python/Homework5/Homework5-WeatherForecast.py
+++ python/Homework5/Homework5-WeatherForecast.py
@@ -1,10 +1,11 @@
-#Brandon Marshall       
+﻿#Brandon Marshall       
 #Python Scripting
 #October 5, 2015
 #Homework 5 - Task 4
 
 import urllib.request
 import json
+import datetime
 from pprint import pprint
 
 #query = input("What location do you want the weather for? ")
@@ -19,14 +20,21 @@ content_string = content.decode("utf-8")
 json_data = json.loads(content_string)
 
 count = 0
+query = query.replace("%20", " ")
 print("Weather Forecast for " + query + '\n');
 
 try:
+	curDay = 0
 	while (True):
 		time = json_data["list"][count]["dt_txt"]
+		dt = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+		if curDay != dt.day:
+			print(dt.strftime('\n%b %d:'))
+			curDay = dt.day
+		formattedDT = dt.strftime('%H:%M:%S')
 		main = json_data["list"][count]["weather"][0]["main"]
 		desc = json_data["list"][count]["weather"][0]["description"]
-		print(time + " " + main + " - " + desc)
+		print(formattedDT + " " + main + " - " + desc)
 		count = count + 1
 except ValueError:
 	print("The end.")
