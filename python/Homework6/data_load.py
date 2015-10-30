d9e52d1cb5c8d0c59d665afc72f3faeeaa70fb30
bmarshall <bmarshallpf@gmail.com>
2015-10-29 23:18:59 -0400

    homework 6 search engine update

---
+++ python/Homework6/data_load.py
@@ -0,0 +1,32 @@
+﻿#Brandon Marshall       
+#Python Scripting
+#October 1, 2015
+#Homework 5 - Web Crawler
+
+import os
+import fnmatch
+import pickle
+
+def get_traversal_data():
+	found = False
+	foundFiles = []
+
+	start_dir = "C:\\Users\\Marshall\\Documents\\School\\PythonProgramming\\fortune1"
+
+	if (os.path.exists(start_dir)):
+		for dirpath, dirs, files in os.walk(start_dir):
+			for single_file in files:
+				if fnmatch.fnmatch(single_file, "*txt") or fnmatch.fnmatch(single_file, "*log"):
+					f = open(os.path.join(dirpath, single_file), "r")
+					foundFiles.append([(os.path.abspath(single_file), f.read())])
+					found = True
+		if not found:
+			print("Sorry - the file was not found.")
+	else:
+		print("Sorry - the path does not exist.")
+
+	return foundFiles
+
+	#out = open("raw_data.pickle", "bw")
+	#pickle.dump(foundFiles, out)
+	#out.close()
