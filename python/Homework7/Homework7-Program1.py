8bc4bff0201d7119713f1837b683365d1614e431
bmarshall <bmarshallpf@gmail.com>
2015-11-11 20:02:27 -0500

    added data to request

--- python/Homework7/Homework7-Program1.py
+++ python/Homework7/Homework7-Program1.py
@@ -4,33 +4,62 @@
 #Homework 7 - Program 1
 
 import psutil, datetime
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.request import urlopen
 
+# retrieve data
 boot_time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-
-print("\nBOOT TIME:", boot_time)
-
 cpu_util = psutil.cpu_percent(interval=1, percpu=True)
-
-i=1
-print("\nCPU UTILIZATION:")
-for cpu in cpu_util:
-	print("CPU {} : {}%".format(i, cpu))
-	i+=1
-
-
 mem = psutil.virtual_memory()
 THRESHOLD = 100 * 1024 * 1024  # 100MB
-print("\nAVAILABLE MEMORY:", mem.available)
-print("USED MEMORY:", mem.used)
-print("USED PERCENTAGE:", mem.percent)
+users = psutil.users()
+cpu_percent = psutil.cpu_percent()
+pids = psutil.pids()
+partitions = psutil.disk_partitions()
 
-# Added by Brandon
-print("\nUSERS: ")
-for usr in psutil.users():
-	print(usr)
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+	def do_GET(self): 
+		self.send_response(200)
+		self.end_headers()
 
-print("\nPIDS: ", psutil.pids())
+		text = "\nBOOT TIME: " + str(boot_time)
+		self.wfile.write(str.encode(text))
 
-print("\nDISK PARTITIONS: ")
-for part in psutil.disk_partitions():
-	print(part)
+		text = "\n\nCPU UTILIZATION:"
+		self.wfile.write(str.encode(text))
+		i=1
+		for cpu in cpu_util:
+			text = "\nCPU {} : {}%".format(i, cpu)
+			self.wfile.write(str.encode(text))
+			i+=1
+
+		text = "\n\nAVAILABLE MEMORY: " + str(mem.available)
+		self.wfile.write(str.encode(text))
+		text = "\n\nUSED MEMORY: " + str(mem.used)
+		self.wfile.write(str.encode(text))
+		text = "\n\nUSED PERCENTAGE: " + str(mem.percent) + "%"
+		self.wfile.write(str.encode(text))
+
+		# Added by Brandon
+		text = "\n\nUSERS (name): "
+		self.wfile.write(str.encode(text))
+		for user in users:
+			text = str(user.name)
+			self.wfile.write(str.encode(text))
+
+		text = "\n\nCPU PERCENT: " + str(cpu_percent) + "%"
+		self.wfile.write(str.encode(text))
+
+		text = "\n\nPIDS:\n" + str(pids)
+		self.wfile.write(str.encode(text))
+
+		text = "\n\nDISK PARTITIONS:\n"
+		self.wfile.write(str.encode(text))
+		for part in partitions:
+			text = str(part) + "\n"
+			self.wfile.write(str.encode(text))
+
+		return
+
+server = HTTPServer(("", 8000), MyHTTPRequestHandler)
+server.serve_forever()
