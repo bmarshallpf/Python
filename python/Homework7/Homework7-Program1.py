16c68927dd45d73026ae081039105e23c65a27d0
bmarshall <bmarshallpf@gmail.com>
2015-11-12 20:48:10 -0500

    final program 1 for HW7

--- python/Homework7/Homework7-Program1.py
+++ python/Homework7/Homework7-Program1.py
@@ -1,11 +1,9 @@
-#Brandon Marshall       
+﻿#Brandon Marshall       
 #Python Scripting
 #November 11, 2015
 #Homework 7 - Program 1
 
 import psutil, datetime
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from urllib.request import urlopen
 
 # retrieve data
 boot_time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
@@ -17,48 +15,49 @@ cpu_percent = psutil.cpu_percent()
 pids = psutil.pids()
 partitions = psutil.disk_partitions()
 
+first = "<tr><td bgcolor=\"green\">"
+middle = "</td><td bgcolor=\"#89C35C\">"
+end = "</td></tr>"
+
+text = "<table border=1>"
+text = text + first + "BOOT TIME" + middle + str(boot_time) + end
+text = text + first + "CPU UTILIZATION" + middle
+i = 1
+for cpu in cpu_util:
+	text = text + "<li>CPU {} : {}%".format(i, cpu) + "</li>"
+	i+=1
+i = 1
+text = text + end
+text = text + first + "AVAILABLE MEMORY" + middle + str(mem.available) + end
+text = text + first + "USED MEMORY" + middle + str(mem.used) + end
+text = text + first + "USED PERCENTAGE" + middle + str(mem.percent) + "%" + end
+text = text + first + "CPU PERCENT" + middle + str(cpu_percent) + "%" + end
+text = text + first + "USERS" + middle + str(users[0].name) + end
+text = text + first + "PIDS (First 10)" + middle
+i = 1
+for pid in pids:
+	text = text + "<li>" + str(pid) + "</li>"
+	i += 1
+	if i > 10:
+		break
+i = 1
+text = text + end
+text = text + first + "DISKS" + middle
+i = 1
+for part in partitions:
+	text = text + "<li>" + str(part.device) + "</li>"
+	i += 1
+text = text + end
+text = text + "</table>"
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.request import urlopen
+
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self): 
 		self.send_response(200)
 		self.end_headers()
-
-		text = "\nBOOT TIME: " + str(boot_time)
 		self.wfile.write(str.encode(text))
-
-		text = "\n\nCPU UTILIZATION:"
-		self.wfile.write(str.encode(text))
-		i=1
-		for cpu in cpu_util:
-			text = "\nCPU {} : {}%".format(i, cpu)
-			self.wfile.write(str.encode(text))
-			i+=1
-
-		text = "\n\nAVAILABLE MEMORY: " + str(mem.available)
-		self.wfile.write(str.encode(text))
-		text = "\n\nUSED MEMORY: " + str(mem.used)
-		self.wfile.write(str.encode(text))
-		text = "\n\nUSED PERCENTAGE: " + str(mem.percent) + "%"
-		self.wfile.write(str.encode(text))
-
-		# Added by Brandon
-		text = "\n\nUSERS (name): "
-		self.wfile.write(str.encode(text))
-		for user in users:
-			text = str(user.name)
-			self.wfile.write(str.encode(text))
-
-		text = "\n\nCPU PERCENT: " + str(cpu_percent) + "%"
-		self.wfile.write(str.encode(text))
-
-		text = "\n\nPIDS:\n" + str(pids)
-		self.wfile.write(str.encode(text))
-
-		text = "\n\nDISK PARTITIONS:\n"
-		self.wfile.write(str.encode(text))
-		for part in partitions:
-			text = str(part) + "\n"
-			self.wfile.write(str.encode(text))
-
 		return
 
 server = HTTPServer(("", 8000), MyHTTPRequestHandler)
