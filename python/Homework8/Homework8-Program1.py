2d888e141640c483909ef68544c506763b0f1792
bmarshall <bmarshallpf@gmail.com>
2015-11-16 21:39:58 -0500

    updates to homework 8

--- python/Homework8/Homework8-Program1.py
+++ python/Homework8/Homework8-Program1.py
@@ -11,19 +11,25 @@ from PIL import ImageTk
 dog1 = "dog.png"
 dog2 = "dog_bark.png"
 cur = dog1
+text1 = "Good Dog!"
+text2 = "Bark! Bark!"
+curText = text1
 
 def buttonReleased(event):
 	global cur, dog1, dog2
 	if cur == dog1:
 		img2 = ImageTk.PhotoImage(file = dog2)
 		cur = dog2
+		curText = text2
 		rand = random.randrange(1, 6)
 		os.system("start dog_bark" + str(rand) + ".wav")
 	else:
 		img2 = ImageTk.PhotoImage(file = dog1)
 		cur = dog1
+		curText = text1
 	panel.configure(image = img2)
 	panel.image = img2
+	panel2.configure(text = curText)
 
 base = Tk()
 w, h = base.winfo_screenwidth(), base.winfo_screenheight()
@@ -34,7 +40,9 @@ frame.pack()
 
 img = ImageTk.PhotoImage(file = dog1)
 panel = Label(frame, image = img)
-panel.pack(side = "bottom", fill = "both")
+panel.pack(side = "top", fill = "both")
 panel.bind("<ButtonRelease-1>", buttonReleased)
+panel2 = Label(base, text = text1, font=("Helvetica", 72))
+panel2.pack(side = "bottom")
 
 mainloop()
