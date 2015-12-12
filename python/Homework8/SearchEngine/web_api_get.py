30075ae0b44344331fdf3c469050f56e1e9e58e4
bmarshall <bmarshallpf@gmail.com>
2015-12-11 22:45:51 -0500

    fixing REST api for searching database

--- python/Homework8/SearchEngine/web_api_get.py
+++ python/Homework8/SearchEngine/web_api_get.py
@@ -1,4 +1,4 @@
-#Brandon Marshall       
+﻿#Brandon Marshall       
 #Python Scripting
 #November 20, 2015
 #Homework 8 - Search Engine
@@ -8,26 +8,31 @@ from searcher import perform_database_search, parse_terms
 
 app=Flask(__name__)
 
-data=[{'fname':'Steve', 'lname':'Jobbs'},{'fname':'Bill', 'lname':'Gates'}]
+#data=[{'fname':'Steve', 'lname':'Jobbs'},{'fname':'Bill', 'lname':'Gates'}]
 
 @app.route('/data', methods=['GET'])
 def get_data():
 	return jsonify({'data' : data})
 
-@app.route('/data/', methods=['GET'])
+@app.route('/data/<int:id>', methods=['GET'])
 def get_data_byid(id):
 	return jsonify({'data' : data[id]})
 
-@app.route('/search/', methods=['GET'])
+@app.route('/search/<search_term>', methods=['GET'])
 def get_results(search_term):
 	values = parse_terms(search_term)
 	terms = values['terms']
 	hasAnd = values['hasAnd']
 	hasOr = values['hasOr']
 	results = perform_database_search(terms, hasOr, hasAnd)
-	data = list()
-	for i, result in enumerate(results):
-		data.append({'result' + str(i), str(result)})
-	return jsonify({'data' : data[search_term]})
+	data = []
+	count = 1
+	for result in results:
+		res_str = str(result)
+		if len(res_str) > 0:
+			temp = { str('Result ' + str(count)): str(result) }
+			data.append(temp)
+			count += 1
+	return jsonify({'Search_Results' : data})
 
 app.run()
